d7be94b12858cd1410b55baf0e84f3d363202df3
tijoy <1306929690@qq.com>
2020-12-05 00:32:06 +0800

    Add files via upload

---
+++ incep_v3_po.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+python attacker_Po_cos_incep_v3.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_v3_TI_po_time/ --loss=po --logname=incep_v3_po.txt --cuda=2
+python attacker_Po_cos_incep_v4.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_v4_TI_po_time/ --loss=po --logname=incep_v4_po.txt --cuda=2
+python attacker_Po_cos_incep_res_v2.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_res_v2_TI_po_time/ --loss=po --logname=incep_res_v2_po.txt --cuda=2
+python attacker_Po_cos_res_v2_50.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_50_TI_po_time/ --loss=po --logname=res_v2_50_po.txt --cuda=2
+python attacker_Po_cos_res_v2_101.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_101_TI_po_time/ --loss=po --logname=res_v2_101_po.txt --cuda=2
+python attacker_Po_cos_res_v2_152.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_152_TI_po_time/ --loss=po --logname=res_v2_152_po.txt --cuda=2
