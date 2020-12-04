5bdde1150bd11897b976693d8b3b89228876abde
tijoy <1306929690@qq.com>
2020-12-05 00:47:20 +0800

    Update incep_v3_ce.sh

--- incep_v3_ce.sh
+++ incep_v3_ce.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-python attacker_Po_cos_incep_v3.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_v3_TI_ce_time/ --loss=ce --logname=incep_v3_ce.txt --cuda=1
-python attacker_Po_cos_incep_v4.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_v4_TI_ce_time/ --loss=ce --logname=incep_v4_ce.txt --cuda=1
-python attacker_Po_cos_incep_res_v2.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-incep_res_v2_TI_ce_time/ --loss=ce --logname=incep_res_v2_ce.txt --cuda=1
-python attacker_Po_cos_res_v2_50.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_50_TI_ce_time/ --loss=ce --logname=res_v2_50_ce.txt --cuda=1
-python attacker_Po_cos_res_v2_101.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_101_TI_ce_time/ --loss=ce --logname=res_v2_101_ce.txt --cuda=1
-python attacker_Po_cos_res_v2_152.py --output_dir=/data/ltj/Pycharm_Projects/Po-Attack/result-res_v2_152_TI_ce_time/ --loss=ce --logname=res_v2_152_ce.txt --cuda=1
+python attacker_Po_cos_incep_v3.py --output_dir=result-incep_v3_TI_ce/ --loss=ce --logname=incep_v3_ce.txt --cuda=1
+python attacker_Po_cos_incep_v4.py --output_dir=result-incep_v4_TI_ce/ --loss=ce --logname=incep_v4_ce.txt --cuda=1
+python attacker_Po_cos_incep_res_v2.py --output_dir=result-incep_res_v2_TI_ce/ --loss=ce --logname=incep_res_v2_ce.txt --cuda=1
+python attacker_Po_cos_res_v2_50.py --output_dir=result-res_v2_50_TI_ce/ --loss=ce --logname=res_v2_50_ce.txt --cuda=1
+python attacker_Po_cos_res_v2_101.py --output_dir=result-res_v2_101_TI_ce/ --loss=ce --logname=res_v2_101_ce.txt --cuda=1
+python attacker_Po_cos_res_v2_152.py --output_dir=result-res_v2_152_TI_ce/ --loss=ce --logname=res_v2_152_ce.txt --cuda=1
